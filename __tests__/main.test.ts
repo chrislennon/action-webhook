6ed7ea7be31a62c86a8858418b9b6ee2af0b3506
Chris Lennon <Chris.Lennon@dazn.com>
2019-08-15 16:07:49 +0200

    adds env var handling

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,12 +1,17 @@
-import { run }  from '../src/main'
+import { handler }  from '../src/main'
+import dotenv from 'dotenv'
+import * as path from 'path'
 
 describe('Basic Tests', () => {
+  beforeAll(() => {
+    dotenv.config(path.resolve(__dirname + './../.env'))
+  });
 
-  it('Sends a message to Slack', async () => {
-    const message = 'Successful test message '
-    const url = process.env['WEBHOOK_URL']
-    const runner = await run(url, message)
-    expect(runner).toEqual(true)
+  it('Sends a message to a webhook', async () => {
+    const url = process.env.WEBHOOK_URL || ''
+    const message = 'Successful test message!'
+    const response = await handler(url, message)
+    expect(response).toEqual(true)
   });
 
 });
